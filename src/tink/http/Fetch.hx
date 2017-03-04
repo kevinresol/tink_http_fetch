b1c2ca22fe367844c1a69cdb97ed1700afb43bb4
Kevin Leung <kevinresol@gmail.com>
2017-03-05 00:23:44 +0800

    Handle redirect

--- src/tink/http/Fetch.hx
+++ src/tink/http/Fetch.hx
@@ -9,6 +9,8 @@ import tink.io.IdealSource;
 import tink.url.Host;
 import tink.Url;
 
+using tink.CoreApi;
+
 class Fetch {
 	
 	static var client(get, null):Client;
@@ -16,25 +18,35 @@ class Fetch {
 	
 	public static function fetch(url:Url, ?options:FetchOptions) {
 		
-		var client = url.scheme == 'https' ? sclient : client;
-		
-		var uri:String = url.path;
-		if(url.query != null) uri += '?' + url.query;
-		
-		var method = GET;
-		var headers = null;
-		var body:IdealSource = Empty.instance;
-		
-		if(options != null) {
-			if(options.method != null) method = options.method;
-			if(options.headers != null) headers = options.headers;
-			if(options.body != null) body = options.body;
-		}
-		
-		return client.request(new OutgoingRequest(
-			new OutgoingRequestHeader(method, url.host, uri, headers),
-			body
-		));
+		return Future.async(function(cb) {
+			
+			var client = url.scheme == 'https' ? sclient : client;
+			
+			var uri:String = url.path;
+			if(url.query != null) uri += '?' + url.query;
+			
+			var method = GET;
+			var headers = null;
+			var body:IdealSource = Empty.instance;
+			
+			if(options != null) {
+				if(options.method != null) method = options.method;
+				if(options.headers != null) headers = options.headers;
+				if(options.body != null) body = options.body;
+			}
+			
+			client.request(new OutgoingRequest(
+				new OutgoingRequestHeader(method, url.host, uri, headers),
+				body
+			)).handle(function(res) {
+				switch res.header.statusCode {
+					case 301 | 302: fetch(res.header.byName('location').sure(), options).handle(cb); // TODO: reconstruct body
+					// TODO: case 307 | 308: 
+					default: cb(res);
+				}
+			});
+			
+		});
 	}
 	
 	static function get_client() {
