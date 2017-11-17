052a20baf24533bbc81f27122271e288e924cf40
Kevin Leung <kevinresol@gmail.com>
2017-11-18 00:00:33 +0800

    Update tests

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -66,7 +66,9 @@ class RunTests {
   #end
   
   function testStatus(url:String, status = 200) {
-    return fetch(url, {client: client}).next(function(res) return assert(res.header.statusCode == status));
+    return fetch(url, {client: client}).next(function(res) {
+      return assert(res.header.statusCode == status);
+    });
   }
   
   function testData(url:String, method:Method) {
